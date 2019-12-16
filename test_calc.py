6feb1d98df01b9889928b63d7102d3064a7665b1
adammendyk <adammendyk@yahoo.com>
2019-12-16 21:36:03 +0100

    testing calc module

--- test_calc.py
+++ test_calc.py
@@ -3,8 +3,23 @@ import unittest
 
 class testAdd(unittest.TestCase):
     '''
-    Tests the add function from calc lib
+    Tests the add and subtract function from calc lib
     '''
 
-    def test_add_int(self):
-        pass
+    def test_plus_int(self):
+        assert calc.plus(3, 6) == 3 + 6
+        assert calc.plus(-2, 4) == -2 + 4
+        assert calc.plus(0, 0) == 0 + 0
+        assert calc.plus(-5, -5) == -5 + (-5)
+        assert calc.plus(-2, 0) == -2 + 0
+
+    def test_minus(self):
+        assert calc.minus(3, 6) == 3 - 6
+        assert calc.minus(-2, 4) == -2 - 4
+        assert calc.minus(0, 0) == 0 - 0
+        assert calc.minus(-5, -5) == -5 - (-5)
+        assert calc.minus(-2, 0) == -2 - 0
+
+
+if __name__ == 'main':
+    unittest.main()
